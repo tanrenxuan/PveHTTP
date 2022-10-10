9ca296ecaa3f8ce2f2184440e297ddd71863cdf3
tanrenxuan <1057305176@qq.com>
2022-10-10 23:26:47 +0800

    refactor: 重构部分函数

--- PveHTTP.py
+++ PveHTTP.py
@@ -3,18 +3,23 @@ from NetworkType import NetworkType
 import requests
 import sys
 import urllib3
+import re
 
 urllib3.disable_warnings()
 
+cidr_pattern = r"^(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\/([1-9]|[1-2]\d|3[0-2])$"
+ipv4_pattern = r"^(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+
 
 class PveHTTP:
-    def __init__(self, host, username, password):
+    def __init__(self, host, username, password, type):
         self.host = host
         self.username = username
         self.password = password
+        self.login_type = type
         self.base_url = 'https://' + host + ':8006'
 
-        login_data = self.ticket()
+        login_data = self._ticket()
         if not login_data:  # 登陆失败，退出程序
             sys.exit(1)
         self.ticket = login_data.get('ticket')
@@ -24,13 +29,13 @@ class PveHTTP:
             'CSRFPreventionToken': self.csrf_token,
         }
 
-    def req(self, api_url, data_only: bool = True, **kwargs):
+    def req(self, api_url: str, description: str, **kwargs) -> (bool, object):
         """
         封装url
-        :param data_only: 若为true，返回只返回数据；若为false，返回整个response
         :param api_url: api接口
-        :param kwargs: 参数
-        :return: data字段
+        :param description: 接口动作描述，最好包含具体参数，比如xx虚拟机启动
+        :param kwargs: 其他参数
+        :return: bool表示是否请求成功 若需要返回值，则object为具体data
         """
         url = self.base_url + api_url
         try:
@@ -38,13 +43,16 @@ class PveHTTP:
                 **kwargs,
                 'verify': False}
             response = requests.request(url=url, **kwargs)
-            if data_only:
-                return response.json().get('data')
-            return response
+            if response.status_code != 200:
+                print(f'{description}失败，错误码为：{response.status_code} '
+                      f'错误原因：{response.reason} '
+                      f'{"具体错误原因：{}".format(response.json().get("errors")) if response.json() and response.json().get("errors") else ""}')
+                return False, response.json().get("data") if response.json() else None
+            return True, response.json().get("data") if response.json() else None
         except Exception as e:
             print(e)
 
-    def ticket(self):
+    def _ticket(self):
         """
         Create or verify authentication ticket
         HTTP: POST /api2/json/access/ticket
@@ -54,39 +62,45 @@ class PveHTTP:
         payload = {
             'username': self.username,
             'password': self.password,
-            'realm': 'pam'
+            'realm': self.login_type
         }
-        response = self.req(api_url, method='post', json=payload)
-        if not response:
-            print("login fail!")
-            return
-        return response
+        _, response_data = self.req(api_url, description=f'{self.username}登录', method='post', json=payload)
+        return response_data
 
-    def get_cluster_status(self):
+    def get_cluster_status(self) -> (bool, object):
         """
         List recent tasks (cluster wide).
         HTTP: GET /api2/json/cluster/status
         :return:
         """
         api_url = "/api2/json/cluster/status"
-        response = self.req(api_url, method='get', headers=self.verify_header)
-        if not response:
-            print("get cluster status fail!")
-        return response
+        ok, response_data = self.req(api_url, description="获取集群状态", method='get', headers=self.verify_header)
+        return ok, response_data
 
-    def get_cluster_tasks(self):
+    def get_cluster_tasks(self) -> (bool, object):
         """
         List recent tasks (cluster wide)
         HTTP: GET /api2/json/cluster/tasks
         :return:
         """
         api_url = "/api2/json/cluster/tasks"
-        response = self.req(api_url, method='get', headers=self.verify_header)
-        if not response:
-            print("get cluster tasks fail!")
-        return response
+        ok, response_data = self.req(api_url, description="获取集群任务", method='get', headers=self.verify_header)
+        return ok, response_data
 
-    def vm_start(self, node: str, vmid: int) -> bool:
+    def vm_list(self, node: str = 'pve') -> (bool, list):
+        """
+        Virtual machine index (per node).
+        HTTP: GET /api2/json/nodes/{node}/qemu
+        :param node: 节点
+        :return:
+        """
+        api_url = f'/api2/json/nodes/{node}/qemu'
+        ok, response_data = self.req(api_url, description=f"获取{node}节点所有虚拟机", method='get', headers=self.verify_header)
+        if ok:
+            return True, [vm['vmid'] for vm in response_data]
+        return False, []
+
+    def vm_start(self, vmid: int, node: str = 'pve') -> bool:
         """
         Start virtual machine
         HTTP: POST /api2/json/nodes/{node}/qemu/{vmid}/status/start
@@ -95,13 +109,14 @@ class PveHTTP:
         :return:
         """
         api_url = f'/api2/json/nodes/{node}/qemu/{vmid}/status/start'
-        response = self.req(api_url, method='post', headers=self.verify_header, data_only=False)
-        if not response or response.status_code != 200:
-            print(f'开启节点{node}的虚拟机{vmid}失败，状态码为{response.status_code}，状态信息为{response.reason}')
+        _, available_vm = self.vm_list()
+        if vmid not in available_vm:
+            print(f'节点{node}下不存在虚拟机{vmid}')
             return False
-        return True
+        ok, _ = self.req(api_url, description=f"开启节点{node}下的虚拟机{vmid}", method='post', headers=self.verify_header)
+        return ok
 
-    def vm_stop(self, node: str, vmid: int) -> bool:
+    def vm_stop(self, vmid: int, node: str = 'pve') -> bool:
         """
         Stop virtual machine. The qemu process will exit immediately.
         Thisis akin to pulling the power plug of a running computer and may damage the VM data.
@@ -112,11 +127,12 @@ class PveHTTP:
         :return:
         """
         api_url = f'/api2/json/nodes/{node}/qemu/{vmid}/status/stop'
-        response = self.req(api_url, method='post', headers=self.verify_header, data_only=False)
-        if not response or response.status_code != 200:
-            print(f'强制停止节点{node}的虚拟机{vmid}失败，状态码为{response.status_code}，状态信息为{response.reason}')
+        _, available_vm = self.vm_list()
+        if vmid not in available_vm:
+            print(f'节点{node}下不存在虚拟机{vmid}')
             return False
-        return True
+        ok, _ = self.req(api_url, description=f"强制关闭节点{node}下的虚拟机{vmid}", method='post', headers=self.verify_header)
+        return ok
 
     def vm_shutdown(self, vmid: int, node: str = 'pve') -> bool:
         """
@@ -129,13 +145,14 @@ class PveHTTP:
         :return:
         """
         api_url = f'/api2/json/nodes/{node}/qemu/{vmid}/status/shutdown'
-        response = self.req(api_url, method='post', headers=self.verify_header, data_only=False)
-        if not response or response.status_code != 200:
-            print(f'关闭节点{node}的虚拟机{vmid}失败，状态码为{response.status_code}，状态信息为{response.reason}')
+        _, available_vm = self.vm_list()
+        if vmid not in available_vm:
+            print(f'节点{node}下不存在虚拟机{vmid}')
             return False
-        return True
+        ok, _ = self.req(api_url, description=f"关闭节点{node}下的虚拟机{vmid}", method='post', headers=self.verify_header)
+        return ok
 
-    def get_available_networks(self, node: str = 'pve', network_type: Optional[NetworkType] = None):
+    def get_available_networks(self, node: str = 'pve', network_type: Optional[NetworkType] = None) -> (bool, list):
         """
         List available networks
         HTTP: GET /api2/json/nodes/{node}/network
@@ -149,14 +166,14 @@ class PveHTTP:
             params = {
                 'type': network_type.value
             }
-        response = self.req(api_url, method='get', headers=self.verify_header, params=params)
-        if not response:
-            print("no certain networks!")
-        return response
+        ok, response_data = self.req(api_url, description=f"获取{network_type.value if network_type is not None else '所有'}类型网络设备", method='get', headers=self.verify_header, params=params)
+        if ok:
+            return True, [network_device['iface'] for network_device in response_data]
+        return False, []
 
     def create_network_bridge(self, iface: str, node: str = 'pve', cidr: Optional[str] = None,
                               gateway: Optional[str] = None, autostart: Optional[bool] = True,
-                              bridge_ports: Optional[str] = None, ):
+                              bridge_ports: Optional[str] = None, ) -> bool:
         """
         创建网络设备linux bridge（不同的网络设备，请求体设置的参数有所区别，由于只需要bridge类型device，type硬编码为NetworkType。bridge）
         :param iface: 名称，最好起类似名称vmbr0、vmbr1等
@@ -169,14 +186,24 @@ class PveHTTP:
         """
         api_url = f'/api2/json/nodes/{node}/network'
         payload = {
-            "iface": iface,
-            "type": NetworkType.bridge.value, # 硬编码为bridge类型
+            "iface": iface,  # no existence judge
+            "type": NetworkType.bridge.value,  # 硬编码为bridge类型
             "autostart": autostart,
         }
         if cidr is not None:
-            pass
-
-        response = self.req(api_url, method='post', headers=self.verify_header, json=payload)
+            if not re.match(cidr_pattern, cidr):
+                print("cidr格式错误，格式为x.x.x.x/d")
+                return False
+            payload['cidr'] = cidr
+        if gateway is not None:
+            if not re.match(ipv4_pattern, gateway):
+                print("gateway格式错误，格式为x.x.x.x")
+                return False
+            payload['gateway'] = gateway
+        if bridge_ports is not None:  # no existence judge
+            payload['bridge_ports'] = bridge_ports
+        ok, _ = self.req(api_url, description=f"创建网络设备：{'iface:{}'.format(iface)}", method='post', headers=self.verify_header, json=payload)
+        return ok
 
     def __str__(self):
         dict_str = ''
@@ -186,11 +213,23 @@ class PveHTTP:
 
 
 if __name__ == '__main__':
-    data = {
+    my_data = {
         'username': 'root',
         'password': '123456',
-        'host': '192.168.247.131'
+        'host': '192.168.247.131',
+        'type': 'pam'
     }
-    p = PveHTTP(**data)
-    print(p.get_available_networks(network_type=NetworkType.bridge))
-
+    lab_data = {
+        'username': 'root',
+        'password': '1qaz@WSX3edc',
+        'host': '10.0.0.110',
+        'type': 'pve'
+    }
+    p = PveHTTP(**my_data)
+    # print(p.get_available_networks(network_type=NetworkType.bridge))
+    # p.create_network_bridge(iface="vmbr111", bridge_ports='ens11')
+    print(p.get_cluster_status())
+    print(p.get_cluster_tasks())
+    # print(p.vm_start(101))
+    print(p.get_available_networks(network_type=NetworkType.eth))
+    p.create_network_bridge(iface="vmbr1111", cidr="1.2.3.4/22")
